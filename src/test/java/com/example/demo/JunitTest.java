4ada5c726ad2b8f1d25c509a5771670eb2d01b5a
Your Name <you@example.com>
2023-06-04 11:46:49 +0300

    dfs

--- src/test/java/com/example/demo/JunitTest.java
+++ src/test/java/com/example/demo/JunitTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.Test;
 public class JunitTest {
 
     @Test
-    public void what is () {
+    public void testAdd() {
         MyFunctionTest myFunction = new MyFunctionTest();
-        int result = myFunction.add(2, 2);
+        int result = myFunction.add(3, 2);
         assertEquals(5, result);
     }
 
